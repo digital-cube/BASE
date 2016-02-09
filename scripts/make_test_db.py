d7447a1f47c28aabfb58a4ebef1ce577516c2470
Slobodan Dolinic <slobodan@digitalcube.rs>
2016-02-09 14:06:05 +0100

    create test db

--- scripts/make_test_db.py
+++ scripts/make_test_db.py
@@ -25,9 +25,33 @@ db_name = sys.argv[1]
 db_user = sys.argv[2]
 db_pwd = sys.argv[3]
 
+db_app = 'mysql'
 dump_app = 'mysqldump'
-dump_cmd = '{} -u{} -p{} -d {} > {}/test_{}'.format(
+required_tables = 'sequencers'
+
+dump_cmd = '{} -u{} -p{} -d {} > {}/test_{}.dmp'.format(
     dump_app, db_user, db_pwd, db_name, dbm_file_path, db_name
 )
-
+# crate sql schema dump for test
 os.system(dump_cmd)
+
+dump_required_tables_cmd = '{} -u{} -p{} {} -t {} >> {}/test_{}.dmp'.format(
+    dump_app, db_user, db_pwd, db_name, required_tables, dbm_file_path, db_name
+)
+# add required tables to dump
+os.system(dump_required_tables_cmd)
+
+
+destroy_test_db_cmd = "{} -u{} -p{} -e 'drop database if exists test_{}'".format(db_app, db_user, db_pwd, db_name)
+create_test_db_cmd = "{} -u{} -p{} -e 'create database test_{}'".format(db_app, db_user, db_pwd, db_name)
+# destroy test db if exists
+os.system(destroy_test_db_cmd)
+# create test databse
+os.system(create_test_db_cmd)
+
+fill_test_db_cmd = "{} -u{} -p{} test_{} < {}/test_{}.dmp".format(
+    db_app, db_user, db_pwd, db_name, dbm_file_path, db_name
+)
+
+# create test db tables
+os.system(fill_test_db_cmd)
