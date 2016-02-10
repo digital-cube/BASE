983a5d1a4383c921ea6141499dfe411035592f41
Slobodan Dolinic <slobodan@digitalcube.rs>
2016-02-10 10:40:48 +0100

    PATCH added

--- base_lookup/methods_mapping.py
+++ base_lookup/methods_mapping.py
@@ -1,10 +1,11 @@
-from base_lookup.http_methods import GET, PUT, DELETE, POST
+from base_lookup.http_methods import GET, PUT, DELETE, POST, PATCH
 
 method_map = {
     GET: 'do_get',
     PUT: 'do_put',
     DELETE: 'do_delete',
     POST: 'do_post',
+    PATCH: 'do_patch',
 }
 
 method_map_rev = dict((y, x) for (x, y) in method_map.items())
