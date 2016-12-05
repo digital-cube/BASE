759f4d62dceed9ca418601db270ee32d4743142d
Slobodan Dolini? <slobodan@digitalcube.rs>
2016-12-05 09:42:46 +0100

    tests

---
+++ base/tests/run_all_tests.py
@@ -0,0 +1,12 @@
+import unittest
+from tests.test_execute_builder_cmd import TestExecute_builder_cmd
+from tests.test_execute_builder_cmd import TestExecute_builder_cmd_init_fail
+from tests.test_execute_builder_cmd import TestExecute_builder_cmd_init_done
+# from tests.test_execute_builder_cmd import TestExecute_builder_cmd_init_done_with_destination
+# from tests.test_execute_builder_cmd import TestExecute_builder_cmd_init_done_with_description
+from tests.test_execute_builder_cmd import TestExecute_builder_cmd_db_init_fail
+
+
+if __name__ == '__main__':
+
+    unittest.main()
