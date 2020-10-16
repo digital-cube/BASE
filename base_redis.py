9671f8c4248efa04e573c8f40eec439dc73c8ab6
Igor Jeremic <igor@digitalcube.rs>
2020-10-16 10:58:01 +0200

    fix

--- base_redis.py
+++ base_redis.py
@@ -3,33 +3,19 @@ import os
 
 
 class Redis(redis.Redis):
-    pass
+    def __init__(self, *args, **kwargs):
+        redis_host = os.getenv('REDIS_HOST', 'localhost')
+        redis_port = os.getenv('REDIS_PORT', '6379')
 
- #    def __init__(self, *args, **kwargs):
- #        if 'password' not in kwargs:
- #            self.redis_password = os.getenv('REDIS_PASSWORD')
- # #           if not self.redis_password:
- # #               raise NameError("REDIS_PASSWORD environment variable is required")
- #
- #            if self.redis_password:
- #                kwargs['password'] = self.redis_password
- #        else:
- #            self.redis_password = kwargs['password']
- #
- #        if 'port' not in kwargs:
- #            self.redis_port = os.getenv('REDIS_PORT')
- #            if self.redis_port:
- #                kwargs['port']=self.redis_port
- #
- #        if 'host' not in kwargs:
- #            self.redis_host = os.getenv('REDIS_HOST')
- #            if self.redis_host:
- #                kwargs['host']=self.redis_host
- #
- #        super().__init__(*args, **kwargs)
- #
- #    def __del__(self):
- #        if self.redis_password:
- #            super().__del__()
- #
- #    pass
+#        print("BASE::REDIS_INIT_ON(",redis_host, redis_port,")")
+
+        if 'host' not in kwargs:
+            kwargs['host'] = redis_host
+
+        if 'port' not in kwargs:
+            kwargs['port'] = redis_port
+
+        super().__init__(*args, **kwargs)
+
+    def __del__(self):
+        super().__del__()
