9c801f3988f55f1e3eef656543f0e3899867979a
ooblife <ooblife@m.ooblife.com>
2020-11-15 22:42:33 +0100

    fix

--- scripts/mailer.py
+++ scripts/mailer.py
@@ -6,7 +6,7 @@ Send mail runner
 import os
 import sys
 import time
-port = 8801 if len(sys.argv) == 0 else int(sys.argv[1])
+port = 8801 if len(sys.argv) == 1 else int(sys.argv[1])
 while True:
     os.system('python3 send_mail.py {} -s'.format(port))
     time.sleep(1)
