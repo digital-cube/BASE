a7b4b82115002ddca70a1e207f3bd8b7120d7b7f
Igor Jeremic <igor@digitalcube.rs>
2021-01-19 14:42:40 +0100

    fix

--- base/src/base/utils/ipc.py
+++ base/src/base/utils/ipc.py
@@ -6,6 +6,64 @@ from base import http
 import logging
 import os
 
+
+
+
+async def call_TTPlus(request, service, method, endpoint, body=None, readonly=False):
+    method = method.upper()
+
+    if readonly and method != 'GET':
+        raise http.HttpInternalServerError(id_message='IPC_READONLY_SUPPORTED_ONLY_FOR_GET_METHOD',
+                                           message='Readonly flag for not get metod')
+
+    import base
+
+    prefix = base.config.conf['services'][service]['prefix']
+    host = base.config.conf['host']
+
+    if not base.registry.test:
+        port = base.config.conf['port']
+    else:
+        port = base.registry.test_port
+
+    uri = 'http://' + host + ':' + str(port) + prefix + ('/' if endpoint[0] != '/' else '') + endpoint
+
+    http_client = AsyncHTTPClient()
+    headers = {}
+
+    if request and request.headers and base.config.conf['authorization']['key'] in request.headers:
+        headers[base.config.conf['authorization']['key']] = request.headers[base.config.conf['authorization']['key']]
+
+    _body = None if method in ('GET', 'DELETE') else json.dumps(body if body else {}, ensure_ascii=False)
+
+    logging.getLogger('ipc').log(level=logging.DEBUG, msg=f"{method}:{uri}")
+    try:
+        result = await http_client.fetch(uri, method=method, headers=headers, body=_body)
+        logging.getLogger('ipc').log(level=logging.DEBUG, msg=f"OK {method}:{uri}")
+
+    except Exception as e:
+        logging.getLogger('ipc').log(level=logging.CRITICAL, msg=f"FAILED {method}:{uri}")
+        logging.getLogger('ipc').log(level=logging.CRITICAL, msg=f"FAILED body: {_body}")
+        logging.getLogger('ipc').log(level=logging.CRITICAL, msg=f"FAILED response-code: {e.response.code}")
+        logging.getLogger('ipc').log(level=logging.CRITICAL, msg=f"FAILED response-body: {e.response.body}")
+        print(f"\nIPC FAILED, {e}\n")
+
+        try:
+            resp_body = json.loads(e.response.body)
+            message, id_message, code = resp_body['message'], resp_body['id'], resp_body['code']
+        except:
+            raise http.HttpInternalServerError
+
+        raise http.BaseHttpException(message=message,
+                                     id_message=id_message,
+                                     status=code)
+
+    return json.loads(result.body.decode('utf-8'))
+
+
+
+
+
 async def call(request, service, method, endpoint, body=None, readonly=False):
     method = method.upper()
 
