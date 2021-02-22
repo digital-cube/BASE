8d63f6eef188c3b165fcc4fdbf50bbdc90effe14
Igor Jeremic <igor@digitalcube.rs>
2021-02-22 22:43:49 +0100

    fix

--- base/src/base/utils/ipc.py
+++ base/src/base/utils/ipc.py
@@ -76,7 +76,7 @@ async def call(request, service, method, endpoint, body=None, readonly=False):
 
     logging.getLogger('ipc').log(level=logging.DEBUG, msg=f"{method}:{uri}")
     try:
-        result = await http_client.fetch(uri, method=method, headers=headers, body=_body)
+        result = await http_client.fetch(uri, method=method, headers=headers, body=_body, request_timeout=600)
         logging.getLogger('ipc').log(level=logging.DEBUG, msg=f"OK {method}:{uri}")
 
     except Exception as e:
