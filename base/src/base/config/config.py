67d5570fe5df344f272b126da9a1f6929bc0c03e
Slobodan Dolinic <slobodan@digitalcube.rs>
2021-07-07 11:35:23 +0200

    load configs from an array

--- base/src/base/config/config.py
+++ base/src/base/config/config.py
@@ -5,6 +5,7 @@ import logging.config
 
 import asyncio
 import aiotask_context as context
+from typing import Union
 
 
 class config:
@@ -14,7 +15,7 @@ class config:
     initialized = False
 
     @staticmethod
-    def load_from_dict(config_dictionary: dict) -> None:
+    def load_from_dict_old(config_dictionary: dict) -> None:
         """
         Load App configuration from a dictionary
 
@@ -34,7 +35,7 @@ class config:
         # config.initialized = True
 
     @staticmethod
-    def __parse_yaml(path: str) -> dict:
+    def __parse_yaml_old(path: str) -> dict:
         """
         Helper function for parsing a YAML file and returning a dictionary created from the parsed YAML file
 
@@ -69,7 +70,7 @@ class config:
         return config_yaml
 
     @staticmethod
-    def load_from_yaml(path: str) -> None:
+    def load_from_yaml_old(path: str) -> None:
         """
         Function which loads the Application from the
         :param path:
@@ -95,6 +96,125 @@ class config:
         from ..registry import register
         register(config.conf)
         # config.initialized = True
+
+    @staticmethod
+    def load_from_dict(config_dictionary: dict) -> None:
+        """
+        Load App configuration from a dictionary
+
+        :param config_dictionary: The dictionary with the settings for the App.
+        """
+
+        if config.initialized:
+            return
+
+        config.load_default_options()
+
+        config.update_conf(config_dictionary)
+
+        from ..registry import register
+        register(config_dictionary)
+        config.initialized = True
+
+    @staticmethod
+    def __parse_yaml(path: str) -> dict:
+        """
+        Helper function for parsing a YAML file and returning a dictionary created from the parsed YAML file
+
+        :param path: Path to the YAML file
+        :return: Dictionary createad from the YAML file
+        """
+        import yaml
+
+        # Load App config from config.yaml
+        try:
+            with open(path) as file:
+                config_yaml = yaml.load(file, Loader=yaml.SafeLoader)
+        except Exception as e:
+            print(e)
+            sys.exit()
+
+        if config_yaml is None:
+            return {}
+
+        def use_value_from_env(d):
+            '''
+            Read value for configuration from an environment variable with the same name as in the config file
+            :param d: dict - dictionary of settings from yaml file or nested configuration
+            :return: void
+            '''
+            if not d:
+                d = {}
+                return
+
+            for key in d:
+                v = d[key]
+                if type(v) == dict:
+                    use_value_from_env(v)
+                elif type(v) == str and len(v) > 0 and v[0] == '$':
+                    d[key] = os.getenv(v[1:], '')
+
+        use_value_from_env(config_yaml)
+        # trace if client defined it's own configuration
+        config_yaml["default_config"] = True
+
+        return config_yaml
+
+    @staticmethod
+    def update_conf(new_conf) -> None:
+        # config.conf.update(new_conf)
+
+        def _update_conf(old_conf, new_conf):
+            for k in new_conf:
+                if k not in old_conf:
+                    old_conf[k] = new_conf[k]
+                else:
+                    value_type = type(new_conf[k])
+                    if value_type != dict:
+                        if value_type == list:
+                            if old_conf[k]:
+                                old_conf[k].extend(new_conf[k])
+                            else:
+                                old_conf[k] = new_conf[k]
+                        else:
+                            old_conf[k] = new_conf[k]
+                    else:
+                        _update_conf(old_conf[k], new_conf[k])
+
+        _update_conf(config.conf, new_conf)
+
+    @staticmethod
+    def load_from_yaml(paths: Union[str, list]) -> None:
+        """
+        Function which loads the Application from the
+        :param path:
+        """
+
+        # if config.initialized:    # todo: check for this
+        #     return
+
+        config.load_default_options()
+
+        if type(paths) == str:
+            config.update_conf(config.__parse_yaml(paths))
+        else:
+            for path in paths:
+                config.update_conf(config.__parse_yaml(path))
+
+        if 'keys' in config.conf:
+            try:
+                with open(config.conf['keys']['private']) as pkey:
+                    config.conf['keys']['private'] = pkey.read()
+                with open(config.conf['keys']['public']) as pubkey:
+                    config.conf['keys']['public'] = pubkey.read()
+            except Exception as e:
+                print(e)
+                sys.exit()
+
+        from ..registry import register
+        register(config.conf)
+        config.initialized = True
+        config.conf["default_config"] = False
 
     @staticmethod
     def load_default_options():
