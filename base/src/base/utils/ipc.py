cba95a1f684a8c17e1cde07f85638cc2e0ae10a3
Igor Jeremic <igor@digitalcube.rs>
2021-10-16 17:22:39 +0200

    fix

--- base/src/base/utils/ipc.py
+++ base/src/base/utils/ipc.py
@@ -99,8 +99,33 @@ async def call_new(request, service, method, endpoint, body=None):
             logging.getLogger('base').log(level=logging.CRITICAL, msg=f"FAILED {e}")
 
 
+def lprint(s):
+    import os
+    from base import config as bconfig
+
+    svcname = bconfig.conf["name"]
+
+    __pfx = '/logs/' if os.getenv('ENVIRONMENT', 'local') == 'docker' else '/tmp/'
+
+    with open(__pfx + 'trace.log', 'at') as f:
+        f.write(f'{s}\n'.replace("__svcname__", f'{svcname[:10]:>10} '))
+
+    with open(__pfx + f'trace.{svcname}.log', 'at') as f:
+        f.write(f'{s}\n')
+
+
 async def call(request, service, method, endpoint, body=None, readonly=False):
     method = method.upper()
+
+    from base.src.base.app import __indent__
+    import datetime
+    indent = __indent__ * '.'
+    import uuid
+    __id = f'ipc {str(uuid.uuid4())[:4]}'
+    from base import config
+    fname = f'{config.conf["name"]} calling {service}'
+
+    lprint(f"{str(datetime.datetime.now()):>30} {' ':>10} __svcname__ in    : {__id} {indent} > {fname}")
 
     if readonly:
         readonly_supported = os.getenv('USE_READ_REPLICA', 'no').strip().lower() in ('yes', 'true', '1')
@@ -174,9 +199,15 @@ async def call(request, service, method, endpoint, body=None, readonly=False):
         # print("URI",method,uri)
 
         result = await http_client.fetch(uri, method=method, headers=headers, body=_body, request_timeout=600)
+
+        lprint(f"{str(datetime.datetime.now()):>30} {' ':>10} __svcname__ out   : {__id} {indent} < {fname} (OK) {'/'.join(uri.split('/')[3:]).split('?')[0]}")
+
         logging.getLogger('ipc').log(level=logging.DEBUG, msg=f"OK {method}:{uri}")
 
     except Exception as e:
+
+        lprint(f"{str(datetime.datetime.now()):>30} {' ':>10} __svcname__ out   : {__id} {indent} < {fname} (ERR) {e}")
+
         print('EXCEPT IPC', uri)
         logging.getLogger('ipc').log(level=logging.CRITICAL, msg=f"FAILED {method}:{uri}")
         # logging.getLogger('ipc').log(level=logging.CRITICAL, msg=f"FAILED body: {_body}")
